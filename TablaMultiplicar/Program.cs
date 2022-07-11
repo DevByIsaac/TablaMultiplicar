5de1394152eecf722403e692a6a81d58fa0eade8
Isaac Gómez <isaac_gomez_elmaster@live.com>
2022-07-10 22:22:01 -0500

    Agregar archivos de proyecto.

---
+++ TablaMultiplicar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tablaMultiplicar
+{
+    class TablaMultiplicar
+    {
+        static void Main(String[] argumentos)
+        {
+            int x;
+            string y;
+
+            Console.Write("Ingrese el número a multiplicar: \n");
+            y = Console.ReadLine();
+            x = int.Parse(y);
+            
+            if (x >= 1 && x<=12)
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    Console.Write(i + " * " + x + " = " + i * x + "\n");    
+                }
+            }
+            else
+            {
+                Console.Write("Debe ingresar un número entre 1 y 12");
+
+                }
+            Console.ReadKey();
+        }
+}
+}
